e0ef72806dc10f8c8ad1a850c0b159fa96cc3e25
Ninibeth Teixeira <ninibethreina@gmail.com>
2020-01-17 01:14:17 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -48,7 +48,7 @@ class Song
     song 
   end 
   
-  def self.create_from_filename 
+  def self.create_from_filename(name)
     song=self.new 
     song.name= (name.split(" - ")[1].chomp(".mp3"))
     song.artist_name= (name.split(" - ")[0])
